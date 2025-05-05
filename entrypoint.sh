a59e18a4828a9cca48d89a35dd465ad39d893a32
Nirupma-Verma <113432409+Nirupma-Verma@users.noreply.github.com>
2025-05-05 12:06:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,17 +35,44 @@ create_pem() {
 conjur_authn() {
 
 	if [[ -n "$INPUT_AUTHN_ID" ]]; then
-
-		echo "::debug Authenticate via Authn-JWT"
-		JWT_TOKEN=$(curl -H "Authorization:bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL" | jq -r .value )
+	  echo "::debug Authenticate via Authn-JWT"
+	  JWT_TOKEN=$(curl -H "Authorization:bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL" | jq -r .value )
+	 # Parse payload body
+	  j_body=$( echo "$JWT_TOKEN" | cut -d "." -f 2 )
+	  # Repad b64 token (dirty)
+	  padd="=="
+	  jwt_padded="${j_body}${padd}"
+	  #decode payload body
+	  payload=$(echo "$jwt_padded" | base64 -d)
+	  # capture IAT time
+	  iat=$( echo "$payload" | jq .iat )
         
-		if [[ -n "$INPUT_CERTIFICATE" ]]; then
-            echo "::debug Authenticating with certificate"
-			token=$(curl --cacert "conjur_$INPUT_ACCOUNT.pem" --request POST "$INPUT_URL/authn-jwt/$INPUT_AUTHN_ID/$INPUT_ACCOUNT/authenticate" --header "Content-Type: application/x-www-form-urlencoded" --header "Accept-Encoding: base64" --data-urlencode "jwt=$JWT_TOKEN")
-		else
-            echo "::debug Authenticating without certificate"
-			token=$(curl --request POST "$INPUT_URL/authn-jwt/$INPUT_AUTHN_ID/$INPUT_ACCOUNT/authenticate" --header 'Content-Type: application/x-www-form-urlencoded' --header "Accept-Encoding: base64" --data-urlencode "jwt=$JWT_TOKEN")
-		fi
+	  # Check if IAT less than or equal to server epoch
+          if (( "$iat" <= "$EPOCHSECONDS" )); then
+
+            echo "::debug No delta between iat [$iat] and epoch [$EPOCHSECONDS]"
+            echo "$JWT_TOKEN"
+
+          # check if IAT greater than server epoch, if so, calculate delta and sleep before returning
+          elif (( "$iat" > "$EPOCHSECONDS" )); then
+
+            delta=$(( "$iat" - "$EPOCHSECONDS" ))
+            echo "::debug delta found: iat [$iat] // epoch [$EPOCHSECONDS]; sleeping for $delta seconds"
+            sleep "$delta"
+            echo "$JWT_TOKEN"
+
+          else
+            echo "::debug unhandled problem"
+            exit 1 
+          fi
+
+	  if [[ -n "$INPUT_CERTIFICATE" ]]; then
+	     echo "::debug Authenticating with certificate"
+	     token=$(curl --cacert "conjur_$INPUT_ACCOUNT.pem" --request POST "$INPUT_URL/authn-jwt/$INPUT_AUTHN_ID/$INPUT_ACCOUNT/authenticate" --header "Content-Type: application/x-www-form-urlencoded" --header "Accept-Encoding: base64" --data-urlencode "jwt=$JWT_TOKEN")
+	  else
+	     echo "::debug Authenticating without certificate"
+	     token=$(curl --request POST "$INPUT_URL/authn-jwt/$INPUT_AUTHN_ID/$INPUT_ACCOUNT/authenticate" --header 'Content-Type: application/x-www-form-urlencoded' --header "Accept-Encoding: base64" --data-urlencode "jwt=$JWT_TOKEN")
+	  fi
 
 	else
 		echo "::debug Authenticate using Host ID & API Key"
@@ -71,6 +98,7 @@ array_secrets() {
 }
 
 set_secrets() {
+  if [[ ${SECRETS[@]} ]]; then
     for secret in "${SECRETS[@]}"; do
         IFS='|'
         read -ra METADATA <<< "$secret" # [0]=db/sqlusername [1]=sql_username
@@ -99,10 +127,16 @@ set_secrets() {
         if [[ "${secretVal}" == "Malformed authorization token" ]]; then
             echo "::error::Malformed authorization token. Please check your Conjur account, username, and API key. If using authn-jwt, check your Host ID annotations are correct."
             exit 1
+	elif [[ "${secretVal}" == *"is empty or not found"* ]]; then
+	    echo "::error::${secretVal}"
+	    exit 1
         fi
         echo ::add-mask::"${secretVal}" # Masks the value in all logs & output
         echo "${envVar}=${secretVal}" >> "${GITHUB_ENV}" # Set environment variable
     done
+  else 
+   echo "::error::No secret found for retrieval from Conjur Vault"
+  fi
 }
 
 main "$@"
