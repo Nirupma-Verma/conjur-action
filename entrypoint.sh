5b874b1d7861435cdb4a6db0caa8c0032b698c33
Nirupma-Verma <113432409+Nirupma-Verma@users.noreply.github.com>
2025-06-24 16:49:28 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -92,7 +92,7 @@ conjur_authn() {
 		echo "::debug Authenticate via Authn-JWT"
         JWT_TOKEN=$(handle_git_jwt)
 	echo "::debug JWT_TOKEN: $JWT_TOKEN"
-        echo "$JWT_TOKEN" | base64 -d
+        echo "$JWT_TOKEN" | base64
 		if [[ -n "$INPUT_CERTIFICATE" ]]; then
             echo "::debug Authenticating with certificate"
 			token=$(curl --cacert "conjur_$INPUT_ACCOUNT.pem" --request POST "$INPUT_URL/authn-jwt/$INPUT_AUTHN_ID/$INPUT_ACCOUNT/authenticate" --header "Content-Type: application/x-www-form-urlencoded" --header "x-cybr-telemetry: $encoded" --header "Accept-Encoding: base64" --data-urlencode "jwt=$JWT_TOKEN")
